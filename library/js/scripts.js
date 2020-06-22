00b347bd670e10a2fb21e614d5f3f40292450fd8
dawsonprimer <63021531+dawsonprimer@users.noreply.github.com>
2020-06-22 18:22:18 -0500

    homepage
    This is just my first mockup of the homepage. I still need to finish styling the slick slider, as well as test responsive along with any other bugs that crop up.

---
+++ library/js/scripts.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function($) {
+	
+	// Slick Slider
+	
+	$('.work-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dots: false,
+    arrows: true,
+  }); 
+	
+	
+});
