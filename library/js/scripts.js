c0163e7ff7427c7d5b6c4a158c60ca8615f687ce
Nick Meagher <nick@icepick.co>
2020-06-23 13:05:07 -0500

    Worked on the home page

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -1,13 +1,12 @@
 jQuery(document).ready(function($) {
 	
 	// Slick Slider
-	
 	$('.work-slider').slick({
     infinite: true,
     slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToScroll: 1,
     dots: false,
-    arrows: true,
+    arrows: true
   }); 
 	
 	
