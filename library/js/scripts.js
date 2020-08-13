1f6be961a7a23a9fa17dba2deed108fbd2454fd5
dawsonprimer <63021531+dawsonprimer@users.noreply.github.com>
2020-08-13 16:13:03 -0500

    Responsive and Nav Fixes
    There's still some issues with nav. Namely need to come up with design choices in regards to the toggle when the user hasn't scrolled down any. I added in a plain purple background to the collapse, which works well on the homepage but sticks out on other pages due to the gradient. Beyond this, the CTA still needs to be looked at for responsive and the modal needs to be finished.

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -7,7 +7,31 @@ jQuery(document).ready(function($) {
     slidesToScroll: 1,
     dots: false,
     arrows: true
+  });
+    //caches a jQuery object containing the header element
+  var body = $(".navbar-light");
+  $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+
+      if (scroll >= 140) {
+          body.removeClass('navbar-light').addClass("nav-scroll");
+      } else {
+          body.removeClass("nav-scroll").addClass('navbar-light');
+      }
+  }); 
+		//Swaps Navbrand img src on scroll
+	
+  $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+
+      if (scroll >= 140) {
+          $("#primer-logo").attr("src","file:///Users/dawsonwear/Documents/GitHub/PrimerPartnerships/library/images/primer-logo-scroll.svg");
+      } else {
+          $("#primer-logo").attr("src","file:///Users/dawsonwear/Documents/GitHub/PrimerPartnerships/library/images/primer-logo.svg");
+      }
   }); 
 	
 	
 });
+
+//Nav Scrolling
